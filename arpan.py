1777099c2e88118e89fb2a7a04606e1c416a23d2
PrasanaVenkateshS <prasana.seshadri@gmail.com>
2025-06-05 09:42:15 -0400

    add home page

---
+++ arpan.py
@@ -0,0 +1,29 @@
+def load_home_tab():
+    st.markdown("## 👋 Welcome to Reg Reimagine!")
+    st.markdown("### *Regulatory Intelligence and Automation to Manage Implementation with Genius and Agility*")
+
+    st.markdown("---")
+    st.subheader("📘 Background")
+    st.markdown("""
+- To meet regulatory compliance and operating license commitments in various jurisdictions, **abc** is required to submit reports to external regulators across the globe.  
+- There is a constant increase in changes to external reporting submissions, driven by either external regulatory changes or internal initiatives by **abc**.  
+- This results in significant costs for **abc** across various teams as we strive for regulatory compliance.  
+- Additionally, there is a notable shortage of SME (Subject Matter Expert) availability.  
+- Typical SMEs across various teams perform the following tasks (non-exhaustive):
+    - Review and summarize regulatory guidance, sometimes translating local language to English.  
+    - Assess impact of changes outlined due to internal or external initiatives.  
+    - Formulate data, functional, and non-functional specifications for the technology team to implement.  
+    - Review **abc** data products and taxonomies to generate mapping rules for regulatory reports.  
+    - Produce code for data mapping rules.  
+    - Formulate and execute testing strategy for the developed logic.
+    """)
+
+    st.markdown("---")
+    st.subheader("🔮 Future Process Blueprint")
+    st.markdown("""
+- **Reg Reimagine** aims to serve as a virtual SME assistant and collaboration platform, leveraging prior regulatory reporting knowledge.  
+- The project team will register initiatives and request support for tasks like impact assessment, data specifications, or rule generation.  
+- The virtual SME will generate drafts for these tasks.  
+- Teams will review and give feedback, refining the SME’s intelligence over time.  
+- This will simplify and accelerate implementation of mandatory regulatory reporting changes.
+    """)
